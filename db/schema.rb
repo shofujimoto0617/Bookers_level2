ce0802b6aa7c8d4007c50ebd2923670db274d458
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-28 13:52:09 +0900

    [add]topページ,aboutページ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_033539) do
+ActiveRecord::Schema.define(version: 2020_07_28_043614) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
